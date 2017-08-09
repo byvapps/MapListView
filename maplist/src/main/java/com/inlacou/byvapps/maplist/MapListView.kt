c8f3df6789048bce6e66418017fde6add3ee88c2
Inlacou <inlacou@gmail.com>
2017-08-09 11:55:04 +0200

    Minor change

--- maplist/src/main/java/com/inlacou/byvapps/maplist/MapListView.kt
+++ maplist/src/main/java/com/inlacou/byvapps/maplist/MapListView.kt
@@ -258,7 +258,7 @@ class MapListView<T: MapListElementModel> : FrameLayout {
 	}
 
 	@RequiresPermission(anyOf = arrayOf("android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"))
-	fun setMyLocationEnabled(enabled: Boolean, show: Boolean){
+	fun setMyLocationEnabled(enabled: Boolean, show: Boolean = true){
 		controller.setMyLocationEnabled(enabled, show)
 	}
 }
